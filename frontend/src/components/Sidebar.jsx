cbbc7c8a2aec426eaa72b6298682ca16f993001a
yashsri-28 <Yash.Srivastava@9abusiness.com>
2026-08-12 01:33:38 +0530

    Revert "yash"
    This reverts commit f131fe124e7f2c572f39f74bd6e8c8e5b9527608.

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,10 +3,7 @@
 // // // import dsbrdIcon from "../public/iconImgs/dsbrd_menu-icon.svg";
 
 
-// // function Sidebar({ collapsed }) {      
-
-
-
+// // function Sidebar({ collapsed }) {
 // //   const menuItems = [
 // //     // { name: 'Dashboard', icon: <LayoutDashboard size={20} /> },.
 // //     {
